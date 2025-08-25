64a1ecaf5c39115b2024dda76037f6df68d40fda
chris samuel <cosalcedo@up.edu.ph>
2025-08-25 17:28:04 +0800

    prog: migrated blog models

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = model)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
     text = models.TextField()
-    created_date = models.DatetimeField(default = timezone.now)
+    created_date = models.DateTimeField(default = timezone.now)
     published_date = models.DateTimeField(blank = True, null = True)
 
 def publish(self):
